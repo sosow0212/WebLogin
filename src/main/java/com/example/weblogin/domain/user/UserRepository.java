d003b7ae98c2e10dbe17cda90be251b79f037c52
sosow0212 <sosow0212@naver.com>
2021-12-12 16:28:23 +0900

    로그인 성공

--- src/main/java/com/example/weblogin/domain/user/UserRepository.java
+++ src/main/java/com/example/weblogin/domain/user/UserRepository.java
@@ -3,5 +3,5 @@ package com.example.weblogin.domain.user;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
-
+    User findByUsername(String username);
 }
