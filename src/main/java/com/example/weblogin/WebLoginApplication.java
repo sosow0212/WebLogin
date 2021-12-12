2ee4ad6e1ce3bfb5df610d83fa274872b35b4289
sosow0212 <sosow0212@naver.com>
2021-12-12 15:34:57 +0900

    초기설정

---
+++ src/main/java/com/example/weblogin/WebLoginApplication.java
@@ -0,0 +1,13 @@
+package com.example.weblogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebLoginApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebLoginApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/weblogin/WebLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.weblogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebLoginApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
